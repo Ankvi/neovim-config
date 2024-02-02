b0c50b5c397d12a76f09afb08f7d38fbbae0ad93
Andreas Kvist <kvistcake@gmail.com>
2024-02-02 07:55:46 +0100

    Some error handling when loading launch configs

--- nvim/lua/ankvi/plugins/dap/languages/vscode-launch-configs.lua
+++ nvim/lua/ankvi/plugins/dap/languages/vscode-launch-configs.lua
@@ -1,14 +1,25 @@
 local M = {}
 
+local configurations = {}
+M.has_loaded_configs = false
+
 M.get_launch_configs = function()
+    if M.has_loaded_configs then
+        return configurations
+    end
+
 	local vscodeLaunchConfigPath = vim.fn.getcwd() .. "/.vscode/launch.json"
-	local vscodeLaunchConfig = vim.fn.json_decode(vim.fn.readfile(vscodeLaunchConfigPath))
+    local status, launchFile = pcall(vim.fn.readfile, vscodeLaunchConfigPath)
+
+    if not status then
+        return {}
+    end
+
+	local vscodeLaunchConfig = vim.fn.json_decode(launchFile)
 
 	if vscodeLaunchConfig == nil then
 		return {}
 	end
-
-	local configurations = {}
 
 	if vscodeLaunchConfig.configurations ~= nil then
 		for _, config in ipairs(vscodeLaunchConfig.configurations) do
