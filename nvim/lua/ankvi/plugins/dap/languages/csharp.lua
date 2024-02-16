404c8d45962c65c4a1b6886ffec5f183cd829fb9
Andreas Kvist <kvistcake@gmail.com>
2024-02-16 11:05:28 +0100

    PreLaunchTask for CS debugging

--- nvim/lua/ankvi/plugins/dap/languages/csharp.lua
+++ nvim/lua/ankvi/plugins/dap/languages/csharp.lua
@@ -2,11 +2,19 @@ local M = {}
 
 M.configure_configurations = function()
 	local dap = require("dap")
-	dap.adapters.coreclr = {
-		type = "executable",
-		command = "netcoredbg",
-		args = { "--interpreter=vscode" },
-	}
+	dap.adapters.coreclr = function(callback, config)
+        if config.preLaunchTask then
+            vim.notify("Running " .. config.name .. " task " .. config.preLaunchTask)
+            vim.fn.system(config.preLaunchTask)
+            vim.notify("Finished! Launching configuration: " .. config.name)
+        end
+        local adapter = {
+            type = "executable",
+            command = "netcoredbg",
+            args = { "--interpreter=vscode" },
+        }
+        callback(adapter)
+    end
 
     dap.configurations.cs = {}
 	local projectConfigurations = require("ankvi.plugins.dap.languages.vscode-launch-configs").get_launch_configs()
