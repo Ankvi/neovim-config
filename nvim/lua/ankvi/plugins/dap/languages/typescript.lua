5304200e5e0302494e61b3e9a76e24c76c943fc8
Andreas Kvist <kvistcake@gmail.com>
2024-02-26 13:03:25 +0100

    Specific port for debugging typescript

--- nvim/lua/ankvi/plugins/dap/languages/typescript.lua
+++ nvim/lua/ankvi/plugins/dap/languages/typescript.lua
@@ -1,17 +1,33 @@
 local M = {}
 
 M.setup = function()
-	require("dap-vscode-js").setup({
-        debugger_path = vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter",
-        debugger_cmd = { "js-debug-adapter" },
-		adapters = {
-			"pwa-node",
-			"pwa-chrome",
-			"node-terminal",
-		},
-	})
+	local mason = require("mason-registry")
+	local debugger_path = mason.get_package("js-debug-adapter"):get_install_path()
+	-- local debugger_path = vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter"
+	-- require("dap-vscode-js").setup({
+	--        debugger_path = debugger_path,
+	--        debugger_cmd = { "js-debug-adapter" },
+	-- 	adapters = {
+	-- 		"pwa-node",
+	-- 		"pwa-chrome",
+	-- 		"node-terminal",
+	-- 	},
+	-- })
 
 	local dap = require("dap")
+
+	for _, adapter in ipairs({ "pwa-node", "pwa-chrome", "node-terminal" }) do
+		dap.adapters[adapter] = {
+			type = "server",
+			host = "localhost",
+			port = 8123,
+			executable = {
+				command = debugger_path .. "/js-debug-adapter",
+				args = { 8123 },
+			},
+		}
+	end
+
 	local dap_utils = require("dap.utils")
 
 	local configurations = {
@@ -46,7 +62,7 @@ M.setup = function()
 		},
 	}
 
-    local projectConfigurations = require("ankvi.plugins.dap.languages.vscode-launch-configs").get_launch_configs()
+	local projectConfigurations = require("ankvi.plugins.dap.languages.vscode-launch-configs").get_launch_configs()
 	for _, config in ipairs(projectConfigurations) do
 		table.insert(configurations, config)
 	end
