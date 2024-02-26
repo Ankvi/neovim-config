4eb20cd4dacd508cc09464b5f07e8182e192e8c5
Andreas Kvist <kvistcake@gmail.com>
2024-02-26 07:12:32 +0100

    Dotnet build

--- nvim/lua/ankvi/dotnet/dotnet.lua
+++ nvim/lua/ankvi/dotnet/dotnet.lua
@@ -1,11 +1,20 @@
+--- @class RestoreOpts
+--- @field noCache boolean Don't use cache
+
 --- @class Dotnet
---- @function Restore(opts: RestoreOpts)
 local M = {}
 
+--- Runs dotnet restore
+--- @param opts RestoreOpts
 function M.restore(opts)
     print("Got restore opts: " .. vim.fn.json_encode(opts))
     local result = vim.fn.system("dotnet restore")
     vim.notify(result)
 end
 
+function M.build(opts)
+    vim.fn.system("dotnet build")
+end
+
 return M
+
