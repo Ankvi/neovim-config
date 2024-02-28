e287dfe664322b56fd781a3bb29e3ba411d1f8c3
Andreas Kvist <kvistcake@gmail.com>
2024-02-28 17:06:31 +0100

    Unnecessary arguments to omnisharp

--- nvim/lua/ankvi/plugins/lsp/lspconfig/servers.lua
+++ nvim/lua/ankvi/plugins/lsp/lspconfig/servers.lua
@@ -72,7 +72,6 @@ local get_server_configs = function()
 			end,
 		},
 		omnisharp = {
-			cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
 			enable_roslyn_analyzers = true,
 			enable_import_completion = true,
 			organize_imports_on_format = true,
