e6a4824db8967403aa6c053157d69339deb44775
Andreas Kvist <kvistcake@gmail.com>
2024-02-29 07:29:37 +0100

    Cleanup csharp dap slightly

--- nvim/lua/ankvi/plugins/dap/languages/csharp.lua
+++ nvim/lua/ankvi/plugins/dap/languages/csharp.lua
@@ -2,28 +2,24 @@ local M = {}
 
 M.configure_configurations = function()
 	local dap = require("dap")
-    local vscodeLaunchConfigs = require("ankvi.plugins.dap.languages.vscode-launch-configs")
+	local vscodeLaunchConfigs = require("ankvi.plugins.dap.languages.vscode-launch-configs")
 
-	dap.adapters.coreclr = function(callback, config)
-        local adapter = {
-            type = "executable",
-            command = "netcoredbg",
-            args = { "--interpreter=vscode" },
-        }
-        callback(adapter)
-    end
+	dap.adapters.coreclr = {
+		type = "executable",
+		command = "netcoredbg",
+		args = { "--interpreter=vscode" },
+	}
 
-    dap.configurations.cs = {}
+	dap.configurations.cs = {}
 	local projectConfigurations = vscodeLaunchConfigs.get_launch_configs()
 	for _, config in ipairs(projectConfigurations) do
-        if config.preLaunchTask then
-            local task = vscodeLaunchConfigs.get_task_for_label(config.preLaunchTask)
-            if task ~= nil then
-                local command = task.command .. " " .. table.concat(task.args, " ")
-                config.preLaunchTask = command
-            end
-
-        end
+		if config.preLaunchTask then
+			local task = vscodeLaunchConfigs.get_task_for_label(config.preLaunchTask)
+			if task ~= nil then
+				local command = task.command .. " " .. table.concat(task.args, " ")
+				config.preLaunchTask = command
+			end
+		end
 		table.insert(dap.configurations.cs, config)
 	end
 end
