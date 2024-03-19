8cf74ce3537835259fbd76977d350bdaef8e21fc
Andreas Kvist <kvistcake@gmail.com>
2024-03-19 13:45:28 +0100

    Finally async dotnet functions

--- nvim/lua/ankvi/dotnet/dotnet.lua
+++ nvim/lua/ankvi/dotnet/dotnet.lua
@@ -1,3 +1,23 @@
+local function dotnet(command)
+    local output = ""
+    local err = ""
+    vim.fn.jobstart("dotnet " .. command, {
+        on_stdout = function(_, data, _)
+            output = output .. "\n" .. data
+        end,
+        on_stderr = function(_, data, _)
+            err = err .. "\n" .. data
+        end,
+        on_exit = function(_, code, _)
+            if code == 0 then
+                vim.notify(command .. " successful!\n" .. output)
+            else
+                vim.notify(command .. " failed!\n" .. err)
+            end
+        end,
+    })
+end
+
 --- @class RestoreOpts
 --- @field noCache boolean Don't use cache
 
@@ -7,13 +27,11 @@ local M = {}
 --- Runs dotnet restore
 --- @param opts RestoreOpts
 function M.restore(opts)
-    print("Got restore opts: " .. vim.fn.json_encode(opts))
-    local result = vim.fn.system("dotnet restore")
-    vim.notify(result)
+    dotnet("restore")
 end
 
 function M.build(opts)
-    vim.fn.system("dotnet build")
+    dotnet("build")
 end
 
 return M
