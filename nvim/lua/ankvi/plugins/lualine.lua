c107d0ee76658d64a19923bdd0fa35cc7194ce0d
Andreas Kvist <kvistcake@gmail.com>
2024-03-21 12:56:06 +0100

    nvim tree popup again and updates

--- nvim/lua/ankvi/plugins/lualine.lua
+++ nvim/lua/ankvi/plugins/lualine.lua
@@ -1,14 +1,15 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-        -- "folke/tokyonight.nvim",
-        "catppuccin",
-    },
-    config = function()
-        require("lualine").setup({
-            options = {
-                theme = "catppuccin"
-            }
-        })
-    end
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		-- "folke/tokyonight.nvim",
+		"catppuccin",
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "catppuccin",
+			},
+		})
+	end,
 }
