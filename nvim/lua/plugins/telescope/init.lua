5fc3a61b1022155a1de745ccc9c2c2c80d603d37
Andreas Kvist <kvistcake@gmail.com>
2024-04-15 09:44:37 +0200

    fixed this damn binding

--- nvim/lua/plugins/telescope/init.lua
+++ nvim/lua/plugins/telescope/init.lua
@@ -26,7 +26,7 @@ return {
 
 		vim.keymap.set("n", "<leader>pf", builtin.find_files, opts)
 		vim.keymap.set("n", "<C-p>", builtin.git_files, opts)
-        vim.keymap.set("n", "<C-Tab>", builtin.buffers, opts)
+        vim.keymap.set("n", "<leader><Tab>", builtin.buffers, opts)
 		vim.keymap.set("n", "<leader>ps", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end, opts)
