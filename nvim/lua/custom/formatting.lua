6c02d17ccbd89e7358f893044d14cf613146969f
Andreas Kvist <kvistcake@gmail.com>
2024-08-08 15:46:49 +0200

    removed unused code

--- nvim/lua/custom/formatting.lua
+++ nvim/lua/custom/formatting.lua
@@ -1,25 +1,5 @@
 local conform = require("conform")
-local conform_util = require("conform.util")
--- local lspconfig_util = require("lspconfig.util")
---
--- local biome_or_prettierd = function(bufnr)
--- 	-- local biomeCheck = lspconfig_util.root_pattern("biome.json")
--- 	if conform.get_formatter_info("biome", bufnr).available then
--- 		return { "biome-check" }
--- 	else
--- 		return { "prettierd", "prettier" }
--- 	end
--- end
---
--- local formatter_overrides = {
---     ["biome-check"] = {
---         require_cwd = true
---     },
---     biome = {
---         require_cwd = true
---     }
--- }
---
+
 conform.setup({
 	formatters_by_ft = {
 		sh = { "shfmt" },
@@ -28,16 +8,10 @@ conform.setup({
 		javascriptreact = { "biome-check", "prettierd", "prettier", stop_after_first = true },
 		typescript = { "biome-check", "prettierd", "prettier", stop_after_first = true },
 		typescriptreact = { "biome-check", "prettierd", "prettier", stop_after_first = true },
-		-- javascript = biome_or_prettierd,
-		-- javascriptreact = biome_or_prettierd,
-		-- typescript = biome_or_prettierd,
-		-- typescriptreact = biome_or_prettierd,
 		css = { "prettierd", "prettier" },
 		html = { "prettierd", "prettier" },
 		json = { "biome-check", "prettierd", "prettier", stop_after_first = true },
 		jsonc = { "biome-check" },
-		-- json = biome_or_prettierd,
-		-- jsonc = biome_or_prettierd,
 		yaml = { "prettierd", "prettier" },
 		lua = { "stylua" },
 		python = { "isort", "black" },
