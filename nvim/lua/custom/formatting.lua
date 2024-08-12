f0dee9bdb2e0b91619c7711da538abfdfe0e2ca1
Andreas Kvist <kvistcake@gmail.com>
2024-08-12 07:27:41 +0200

    format on save and adapted config to latest format

--- nvim/lua/custom/formatting.lua
+++ nvim/lua/custom/formatting.lua
@@ -25,31 +25,17 @@ conform.setup({
 			require_cwd = true,
 		},
 	},
+    default_format_opts = {
+        lsp_format = "fallback"
+    },
+    format_on_save = {
+        timeout_ms = 1000
+    }
 })
 
 vim.keymap.set({ "n", "v" }, "<F3>", function()
 	conform.format({
-		lsp_fallback = true,
 		async = false,
 		timeout_ms = 2000,
 	})
 end)
-
-vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(args)
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = augroup,
-			buffer = args.buf,
-			callback = function()
-				-- Format the code before you run fix usings
-				-- vim.lsp.buf.format({ timeout = 1000, async = false })
-
-				-- If the file is C# then run fix usings
-				-- if vim.bo[0].filetype == "cs" then
-				-- 	require("csharp").fix_usings()
-				-- end
-			end,
-		})
-	end,
-})
