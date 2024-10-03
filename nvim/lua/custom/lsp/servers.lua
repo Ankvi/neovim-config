e70820a68ac59040eb0b9285cf4d2f9ad0036648
Andreas Kvist <kvistcake@gmail.com>
2024-10-03 22:35:22 +0200

    cleanup and readd biome conform checks

--- nvim/lua/custom/lsp/servers.lua
+++ nvim/lua/custom/lsp/servers.lua
@@ -40,7 +40,6 @@ local configs = {
     eslint = {},
     html = {},
     jsonls = {
-
         settings = {
             json = {
                 schemas = schemastore.json.schemas(),
@@ -52,36 +51,6 @@ local configs = {
     },
     lemminx = {},
     lua_ls = {},
-    -- lua_ls = {
-    -- 	on_init = function(client)
-    -- 		local path = client.workspace_folders[1].name
-    -- 		if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-    -- 			client.config.settings = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-    -- 				telemetry = { enable = false },
-    -- 				runtime = {
-    -- 					version = "LuaJIT",
-    -- 					-- path = runtime_path,
-    -- 				},
-    -- 				diagnostics = {
-    -- 					globals = { "vim" },
-    -- 				},
-    -- 				workspace = {
-    -- 					checkThirdParty = false,
-    -- 					library = {
-    -- 						vim.fn.expand("$VIMRUNTIME/lua"),
-    -- 						vim.fn.stdpath("config") .. "/lua",
-    -- 					},
-    -- 				},
-    -- 				completion = {
-    -- 					callSnippet = "Replace",
-    -- 				},
-    -- 			})
-    --
-    -- 			client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-    -- 		end
-    -- 		return true
-    -- 	end,
-    -- },
     omnisharp = {
         cmd = { "omnisharp" },
         settings = {
