b4307ee78a43c54e9b7f4a21bd3c518bab3a4ed7
Andreas Kvist <kvistcake@gmail.com>
2024-10-13 19:33:42 +0200

    test out overseer. some mason stuff and lazy updates

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,42 +1,59 @@
 return {
-	"williamboman/mason.nvim",
+    "williamboman/mason.nvim",
 
-	dependencies = {
-		{ "williamboman/mason-lspconfig.nvim" },
-		{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
-	},
+    dependencies = {
+        { "williamboman/mason-lspconfig.nvim" },
+        { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+    },
 
-	build = function()
-		pcall(vim.api.nvim_command, "MasonUpdate")
-	end,
+    build = function()
+        pcall(vim.api.nvim_command, "MasonUpdate")
+    end,
 
-	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			automatic_installation = true,
-		})
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            automatic_installation = true,
+        })
 
-		local tools = { --vim.tbl_extend("keep", vim.tbl_keys(servers.get_server_configs()), {
-			-- LINTERS --
-			"actionlint",
-			"cpplint",
-			"markdownlint",
-			"pylint",
+        local tools = { --vim.tbl_extend("keep", vim.tbl_keys(servers.get_server_configs()), {
+            -- LSPs
+            "arduino_language_server",
+            "bashls",
+            "cssls",
+            "html",
+            "jsonls",
+            "lemminx",
+            "lua_ls",
+            "omnisharp",
+            "powershell_es",
+            "pyright",
+            "pylsp",
+            "tailwindcss",
+            "vimls",
+            "vtsls",
+            "yamlls",
 
-			-- FORMATTERS --
-			"black",
-			"clang-format",
-			"prettierd",
-			"stylua",
-			"shfmt",
-			"csharpier",
+            -- LINTERS --
+            "actionlint",
+            "cpplint",
+            "markdownlint",
+            "pylint",
 
-			-- DEBUGGING --
-			"js-debug-adapter",
-		}
+            -- FORMATTERS --
+            "black",
+            "clang-format",
+            "prettierd",
+            "stylua",
+            "shfmt",
+            "csharpier",
 
-		require("mason-tool-installer").setup({
-			ensure_installed = tools,
-		})
-	end,
+            -- DEBUGGING --
+            "js-debug-adapter",
+        }
+
+        require("mason-tool-installer").setup({
+            ensure_installed = tools,
+        })
+    end,
 }
