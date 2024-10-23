4463868ad4841ea12403a318a4efc0d04c10094d
Andreas Kvist <kvistcake@gmail.com>
2024-10-23 05:32:01 +0200

    remove format on save for now

--- nvim/lua/custom/formatting.lua
+++ nvim/lua/custom/formatting.lua
@@ -28,9 +28,9 @@ conform.setup({
     default_format_opts = {
         lsp_format = "fallback",
     },
-    format_on_save = {
-        timeout_ms = 1000,
-    },
+    -- format_on_save = {
+    --     timeout_ms = 1000,
+    -- },
 })
 
 -- vim.api.nvim_create_autocmd("BufWritePre")
