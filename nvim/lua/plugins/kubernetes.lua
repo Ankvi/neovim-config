af61592d7fbe2fdd0d2735d9d4c4021c385f19e3
Andreas Kvist <kvistcake@gmail.com>
2025-02-16 09:44:33 +0100

    added kubectl.nvim and updated packages

---
+++ nvim/lua/plugins/kubernetes.lua
@@ -0,0 +1,9 @@
+return {
+    "ramilito/kubectl.nvim",
+    config = function()
+        require("kubectl").setup()
+    end,
+    keys = {
+        { "<leader>k", '<cmd>lua require("kubectl").toggle()<cr>' },
+    },
+}
