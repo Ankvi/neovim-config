14a421d0120216ae47cfd7b0f2d82a8cb58ef5d8
Andreas Kvist <kvistcake@gmail.com>
2025-05-08 07:14:36 +0200

    disable http.lua

--- nvim/lua/plugins/http.lua
+++ nvim/lua/plugins/http.lua
@@ -1,10 +1,10 @@
 return {
-	"rest-nvim/rest.nvim",
-	ft = "http",
-	config = function()
-		local rest = require("rest-nvim")
-		rest.setup()
-		vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<CR>")
-		vim.keymap.set("n", "<leader>rl", "<cmd>Rest run last<CR>")
-	end,
+  -- 	"rest-nvim/rest.nvim",
+  -- 	ft = "http",
+  -- 	config = function()
+  -- 		local rest = require("rest-nvim")
+  -- 		rest.setup()
+  -- 		vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<CR>")
+  -- 		vim.keymap.set("n", "<leader>rl", "<cmd>Rest run last<CR>")
+  -- 	end,
 }
